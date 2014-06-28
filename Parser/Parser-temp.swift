8a583179486ca18d6c6571a548941507ca2c51a3
Marat S <remarr@gmail.com>
2014-06-28 16:45:46 +0400

    one more test file

---
+++ Parser/Parser-temp.swift
@@ -0,0 +1,15 @@
+//
+//  Parser-temp.swift
+//  Hawking
+//
+//  Created by Marat S. on 28.06.14.
+//  Copyright (c) 2014 CocoaHeadsMsk. All rights reserved.
+//
+
+import Foundation
+
+class ParserTemp {
+    func parseNewsList(content: String) {
+        
+    }
+}
