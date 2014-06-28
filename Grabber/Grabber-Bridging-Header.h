c63097e8fc83c30be0f4f7f549c9b4b239c5ab59
Marat S <remarr@gmail.com>
2014-06-28 17:55:30 +0400

    afnetworking works ok

---
+++ Grabber/Grabber-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Grabber-Bridging-Header.h
+//  Hawking
+//
+//  Created by Marat S. on 28.06.14.
+//  Copyright (c) 2014 CocoaHeadsMsk. All rights reserved.
+//
+
+#import "AFNetworking/AFNetworking.h"
--- Grabber/Grabber.swift
+++ Grabber/Grabber.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+class Grabber {
+    func GrabList(input: String, success: (a:Array<AnyObject>), failure: (err:NSError)) -> Bool {
+        
+        
+        let manager = AFHTTPRequestOperationManager()
+        manager.requestSerializer.setValue("608c6c08443c6d933576b90966b727358d0066b4", forHTTPHeaderField: "X-Auth-Token")
+          
+            manager.GET( "http://graph.facebook.com",
+                parameters: nil,
+                success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                    println("JSON: " + responseObject.description)
+                },
+                failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                    println("Error: " + error.localizedDescription)
+                })
+            
+
+        
+        if input.hasPrefix("http") {
+            grabNewsList(input, success: success, failure: failure)
+        } else {
+            grabNewsListContent(input, success: success, failure: failure)
+        }
+        
+        
+//        success(a: <#Array<AnyObject>#>)
+//        failure(err: <#String#>)
+        
+        
+        return false
+    }
+    
+    func grabNewsList(url: String, success: (a:Array<AnyObject>), failure: (err:NSError)) {
+        if let listUrl = NSURL.URLWithString(url) {
+            
+        }
+    }
+    
+    func grabNewsListContent(content: String, success: (a:Array<AnyObject>), failure: (err:NSError)) {
+        
+    }
+}
