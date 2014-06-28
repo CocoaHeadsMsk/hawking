93e605cc4bdace3fa512221cf7517904f350f7e7
Aguacero <demdxx@gmail.com>
2014-06-28 15:12:10 +0400

    Add parser target

---
+++ Parser/Parser.h
@@ -0,0 +1,13 @@
+//
+//  Parser.h
+//  Parser
+//
+//  Created by Dmitry Ponomarev on 28/06/14.
+//  Copyright (c) 2014 CocoaHeadsMsk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parser : NSObject
+
+@end
---
+++ Parser/Parser.m
@@ -0,0 +1,13 @@
+//
+//  Parser.m
+//  Parser
+//
+//  Created by Dmitry Ponomarev on 28/06/14.
+//  Copyright (c) 2014 CocoaHeadsMsk. All rights reserved.
+//
+
+#import "Parser.h"
+
+@implementation Parser
+
+@end
