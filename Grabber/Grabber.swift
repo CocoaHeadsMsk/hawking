6b540b73c0fc43e4a739ad02e38af23ab99aa170
Aguacero <demdxx@gmail.com>
2014-06-28 19:12:10 +0400

    Update Grabber, ItemModel

--- Grabber/Grabber.swift
+++ Grabber/Grabber.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Grabber {
     
-    func loadData(url: String, success: (data: String) -> Void, failure: (error: NSError) -> Void) {
+    func _loadData(url: String, success: (data: String) -> Void, failure: (error: NSError) -> Void) {
         let manager = AFHTTPRequestOperationManager()
         manager.responseSerializer = AFHTTPResponseSerializer()
         manager.GET(url,
@@ -22,53 +22,22 @@ class Grabber {
                 failure(error: error)
             })
     }
-
-    func grabList(input: String, success: (a:Array<AnyObject>), failure: (err:NSError)) -> Bool {
-        let manager = AFHTTPRequestOperationManager()
-        manager.requestSerializer.setValue("608c6c08443c6d933576b90966b727358d0066b4", forHTTPHeaderField: "X-Auth-Token")
-          
-            manager.GET( "http://graph.facebook.com",
-                parameters: nil,
-                success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-                    println("JSON: " + responseObject.description)
-                },
-                failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
-                    println("Error: " + error.localizedDescription)
-                })
-            
-
-        return false
-
-
-        if input.hasPrefix("http") {
-            grabNewsList(input, success: success, failure: failure)
-        } else {
-            grabNewsListContent(input, success: success, failure: failure)
-        }
-        
-        
-//        success(a: Array<AnyObject>)
-//        failure(err: String)
-        
-        
-        return false
-    }
     
-    func grabNewsList(url: String, success: (a:Array<AnyObject>), failure: (err:NSError)) {
+    func grabList(#url: String, success: (a:Array<ItemModel>) -> Void, failure: (err:NSError) -> Void) {
         if let listUrl = NSURL.URLWithString(url) {
             
         }
     }
     
-    func grabNewsListContent(content: String, success: (a:Array<AnyObject>), failure: (err:NSError)) {
+    func grabList(#content: String, success: (a:Array<ItemModel>), failure: (err:NSError) -> Void) {
         
     }
     
-    func grabArticle(#url: String, success: (article: ArticleModel), failure: (error: NSError)) -> Bool {
+    func grabArticle(#url: String, success: (article: ArticleModel) -> Void, failure: (error: NSError) -> Void) -> Bool {
         return false
     }
     
-    func grabArticle(#text: String, success: (article: ArticleModel), failure: (error: NSError)) -> Bool {
+    func grabArticle(#content: String, success: (article: ArticleModel) -> Void, failure: (error: NSError) -> Void) -> Bool {
         return false
     }
 }
